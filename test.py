d2dea8dfc95e5052c0ed9689ae97de068b96a9db
TWTM <52894737+TWTM@users.noreply.github.com>
2022-04-19 01:06:02 -0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,86 @@
+import time
+
+grid = [[ 5 , 3 , 0 , 0 , 7 , 0 , 0 , 0 , 0 ],
+    [ 6 , 0 , 0 , 1 , 9 , 5 , 0 , 0 , 0 ],
+    [ 0 , 9 , 8 , 0 , 0 , 0 , 0 , 6 , 0 ],
+    [ 8 , 0 , 0 , 0 , 6 , 0 , 0 , 0 , 3 ],
+    [ 4 , 0 , 0 , 8 , 0 , 3 , 0 , 0 , 1 ],
+    [ 7 , 0 , 0 , 0 , 2 , 0 , 0 , 0 , 6 ],
+    [ 0 , 6 , 0 , 0 , 0 , 0 , 2 , 8 , 0 ],
+    [ 0 , 0 , 0 , 4 , 1 , 9 , 0 , 0 , 5 ],
+    [ 0 , 0 , 0 , 0 , 8 , 0 , 0 , 7 , 9 ],]
+
+# drawing the grid
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0:
+            print()
+            print('- - - - - - - - - - - - ')
+        else:    
+            print()
+        for j in range(len(board)):
+            if j % 3 == 0 or  j == 9:
+                print('| ', end='')
+                
+            print(board[i][j],'', end='')
+    print()
+    print('- - - - - - - - - - - - ')
+
+print_board(grid)
+
+def complete(board):
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == ' 0':
+                return False
+        return True
+
+def valid(position_x, position_y, number):
+
+    for fileira in range(9):
+        # pegar todos os numeros da fileira e tirar eles de cogitação como opção pra aquele lugar
+        value = grid[fileira][position_y] 
+        if number == value:
+            return False
+    
+    for coluna in range(9):
+        # pegar todos os numeros da fileira e tirar eles de cogitação como opção pra aquele lugar
+        value = grid[position_x][coluna] 
+        if number == value:
+            return False
+
+    # pegar todos os numeros do quadrado e tirar eles do array possible
+    quadrant = [int(position_x/3), int(position_y/3)]
+    for fileira in range(quadrant[0] * 3, quadrant[0] * 3 + 3):
+        for coluna in range(quadrant[1] * 3, quadrant[1] * 3 + 3):
+            value = grid[fileira][coluna] 
+        if number == value:
+            return False
+    return True
+
+def solve():
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 0:
+                for number in range(1,10):
+                    if valid(row,col,number):
+                        grid[row][col] = number
+                        solve()
+                        grid[row][col] = 0
+                return False
+    print_board(grid)
+
+
+solve()
+
+
+
+  
+
+
+
+
+
+
+
+
