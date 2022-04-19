79d7dfcc193d525cf401c107c3c72b4876192a13
TWTM <52894737+TWTM@users.noreply.github.com>
2022-04-19 01:18:32 -0300

    Update Backtracking_sudoku.py

--- Backtracking_sudoku.py
+++ Backtracking_sudoku.py
@@ -1,5 +1,3 @@
-import time
-
 grid = [[ 5 , 3 , 0 , 0 , 7 , 0 , 0 , 0 , 0 ],
     [ 6 , 0 , 0 , 1 , 9 , 5 , 0 , 0 , 0 ],
     [ 0 , 9 , 8 , 0 , 0 , 0 , 0 , 6 , 0 ],
@@ -21,58 +19,62 @@ def print_board(board):
         for j in range(len(board)):
             if j % 3 == 0 or  j == 9:
                 print('| ', end='')
-                
             print(board[i][j],'', end='')
     print()
     print('- - - - - - - - - - - - ')
 
 print_board(grid)
 
-# check if the board is complete
 def complete(board):
     for i in range(len(board)):
         for j in range(len(board)):
-            if board[i][j] == ' 0':
+            if board[i][j] == 0:
                 return False
         return True
 
-def valid(position_x, position_y, number):
+def possibilities(position_x, position_y):
+    
+    possibles = [1,2,3,4,5,6,7,8,9]
 
     for fileira in range(9):
         # pegar todos os numeros da fileira e tirar eles de cogitação como opção pra aquele lugar
         value = grid[fileira][position_y] 
-        if number == value:
-            return False
+        if value == 0:
+            continue
+        if value in possibles:
+            possibles.remove(int(value))
     
     for coluna in range(9):
         # pegar todos os numeros da fileira e tirar eles de cogitação como opção pra aquele lugar
         value = grid[position_x][coluna] 
-        if number == value:
-            return False
+        if value == 0:
+            continue
+        if value in possibles:
+            possibles.remove(int(value))
 
     # pegar todos os numeros do quadrado e tirar eles do array possible
     quadrant = [int(position_x/3), int(position_y/3)]
+        
     for fileira in range(quadrant[0] * 3, quadrant[0] * 3 + 3):
         for coluna in range(quadrant[1] * 3, quadrant[1] * 3 + 3):
             value = grid[fileira][coluna] 
-        if number == value:
-            return False
-    return True
+            if value == 0:
+                continue
+            if value in possibles:
+                possibles.remove(int(value))
+
+    return possibles
 
 def solve():
     for row in range(len(grid)):
-        for col in range(len(grid[row])):            
+        for col in range(len(grid[row])):
             if grid[row][col] == 0:
-                # looping through all possible values
-                for number in range(1,10):
-                    if valid(row,col,number):
-                        grid[row][col] = number
-                        # if its valid put it in the grid and call solve with the modified grid
-                        if not solve():
-                        # if solve return false we make the previous location = 0 and try another possibility
-                            grid[row][col] = 0
+                possibles = possibilities(row,col)
+                for number in possibles:
+                    grid[row][col] = number
+                    if not solve():
+                        grid[row][col] = 0
                 return False
     print_board(grid)
 
-# main
 solve()
